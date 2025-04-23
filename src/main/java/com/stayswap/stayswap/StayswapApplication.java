f102d7d54092ae3a47105b17cd35dcdc586f7a34
zzjiho <youngyoungnim@gmail.com>
2025-04-23 22:33:22 +0900

    init

---
+++ src/main/java/com/stayswap/stayswap/StayswapApplication.java
@@ -0,0 +1,13 @@
+package com.stayswap.stayswap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayswapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayswapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stayswap/stayswap/StayswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayswap.stayswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
